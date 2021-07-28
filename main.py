fc3382da3bef91bd06fbb8cfaf2ca96001730698
AlanFerrari <87348265+AlanFerrari@users.noreply.github.com>
2021-07-28 17:18:11 -0300

    Primeiro contato com python

---
+++ main.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    numero = input("Digite um número: ")
+    print(f"O número digitado foi {numero}")
---
+++ variaveis.py
@@ -0,0 +1,29 @@
+if __name__== '__main__':
+    temperatura = 20.5
+    print(type(temperatura))
+
+    temperatura = "Alan"
+    print(type(temperatura))
+
+    listaNomes = ["Agata", "Adan", "Sônia"]
+    print(type(listaNomes))
+
+    idade = 31
+    print(type(idade))
+
+    dicionarioPessoa = {
+        "nome": temperatura,
+        "idade": idade
+    }
+    print(type(dicionarioPessoa))
+
+    numeroComplexo = 10j + 5
+    print(type(numeroComplexo))
+
+    # Comentário no Python
+
+    verificacao = False
+    print(type(verificacao))
+
+    variravelNula = None
+    print(type(variravelNula))
